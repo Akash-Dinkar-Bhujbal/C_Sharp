c52eb51021ed396aa441764462ea2471668d8428
AKASH-DINKAR-BHUJBAL <akashbhujbal97@gmail.com>
2022-08-14 15:07:28 +0530

    1.2 Test

---
+++ 01_C# Fundamentals/TestSet2/TestSet2/Program.cs
@@ -0,0 +1,490 @@
+﻿/* 1. Write a C# program and compute the sum of the digits of an integer. 
+use do...while loop
+Input a number(integer): 12
+Sum of the digits of the said integer: 3    */
+
+//class sample
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int num = Convert.ToInt32(Console.ReadLine());//12
+//        int temp = num, rem, sum=0;
+//        do {
+//            rem = temp % 10;//rem
+//            sum += rem;//sum
+//            temp /= 10;//num
+//        } while (temp > 0);
+//        Console.WriteLine("Sum of the {0} digits: {1}",num,sum);
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/* 2. Write a program in C# Sharp to calculate and print the Electricity bill of a given customer. 
+  The customer id., name and unit consumed by the user should be taken from the keyboard 
+  and display the total amount to pay to the customer. The charge are as follow : 
+Unit 	Charge/unit
+upto 199 	@1.20
+200 and above but less than 400 	@1.50
+400 and above but less than 600 	@1.80
+600 and above 	@2.00
+
+If bill exceeds Rs. 400 then a surcharge of 15% will be charged and the minimum bill should be of Rs. 100/-
+
+Test Data :
+1001
+James
+800
+Expected Output :
+Customer IDNO :1001
+Customer Name :James
+unit Consumed :800
+Amount Charges @Rs. 2.00 per unit : 1600.00
+Surchage Amount : 240.00
+Net Amount Paid By the Customer : 1840.00   */
+
+//class sample
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Enter your Customer Id: ");
+//        int customer_id = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Enter your Customer Name: ");
+//        string name= Console.ReadLine();
+//        Console.WriteLine("Enter your Billing Unit: ");
+//        int unit = Convert.ToInt32(Console.ReadLine());
+//        double charges, uCharges, extraCharges = 0, total;
+
+//        // Charges
+//        if (unit < 200) {
+//            uCharges = 1.20;
+//            charges = (double)unit * uCharges;
+//        }else if (unit < 400) {
+//            uCharges = 1.50;
+//            charges = (double)unit * uCharges;
+//        }
+//        else if (unit < 600) {
+//            uCharges = 1.80;
+//            charges = (double)unit * uCharges;
+//        } else {
+//            uCharges = 2;
+//            charges = (double)unit * uCharges;
+//        }
+
+//        // Surchange Amount
+//        if (charges > 400)
+//        {
+//            extraCharges = charges * 0.15;
+//        }
+
+//        total = charges + extraCharges;
+
+//        Console.WriteLine("Customer IDNO :{0}\r\n" +
+//            "Customer Name :{1}\r\n" +
+//            "unit Consumed :{2}\r\n" +
+//            "Amount Charges @Rs. {3} per unit : {4}\r\n" +
+//            "Surchage Amount : {5}\r\n" +
+//            "Net Amount Paid By the Customer : {6}", 
+//            customer_id, name, unit, uCharges, charges,extraCharges,total);
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/* 3. Write a program in C# Sharp to make such a pattern like a pyramid with numbers increased by 1.
+
+   1 
+  2 3 
+ 4 5 6 
+7 8 9 10    */
+
+//class sample
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        int count = 1;
+
+//        for(int i = 1; i <= n; i++)// rows
+//        {
+//            for(int j=i; j<=n; j++)
+//            {
+//                Console.Write(" ");
+//            }
+//            for (int j = 1; j <= i; j++)
+//            {
+//                Console.Write(count++ + " ");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/*4. Write a program in C# Sharp to display the number in reverse order.
+Test Data :
+Input a number: 12345
+Expected Output :
+The number in reverse order is : 54321  */
+
+//public class Sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());//12345
+//        int rem = 1, rev = 0;
+
+//        while (n > 0)
+//        {
+//            rem = n % 10;//lastDigit
+//            n = n / 10;//Without_lastDigit
+//            rev = (rev * 10) + rem;//reverseNo
+//        }
+
+//        Console.WriteLine("The number in reverse order is : " + rev);//54321
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/* 5. Write a program in C# Sharp to create a function to check whether a number is prime or not.
+ Test Data :
+ Input a number : 31
+ Expected Output :
+ 31 is a prime number   */
+
+//public class Sample
+//{
+//    public static void fun(int n)
+//    {
+//        int flag = 0;
+//        for (int i = 2; i < n / 2; i++)
+//        {
+//            if (n % i == 0)
+//            {
+//                flag = 1;
+//                break;
+//            }
+//        }
+//        if (n < 2)
+//            Console.WriteLine(n + " is neither Prime nor Composite");
+//        else if (flag == 0)
+//            Console.WriteLine(n + " is a Prime number");
+//        else
+//            Console.WriteLine(n + " is a Composite number");
+//    }
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        fun(n);
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/* 6. Create a class "PhotoAlbum" with a private attribute "numberOfPages."
+It should also have a public method "GetNumberOfPages", which will return the number of pages.
+The default constructor will create an album with 16 pages. There will be an additional constructor, 
+    with which we can specify the number of pages we want in the album.
+Create a class "BigPhotoAlbum" whose constructor will create an album with 64 pages.
+Create a test class "AlbumTest" to create an album with its default constructor, 
+    one with 24 pages, a "BigPhotoAlbum" and show the number of pages that the three albums have.   */
+
+class PhotoAlbum
+{
+    private int numberOfPages;// field
+    public int NumberOfPages// property
+    {
+        get { return numberOfPages; }//getter
+        set { numberOfPages = value; }//setter
+    }
+    public PhotoAlbum()//DefaultConstructor
+    {
+        numberOfPages = 16;
+    }
+
+    public PhotoAlbum(int pages)//ParamaterisedConstructor
+    {
+        numberOfPages = pages;
+    }
+}
+
+class BigPhotoAlbum
+{
+    private int numberOfPages;// field
+    public int NumberOfPages// property
+    {
+        get { return numberOfPages; }//getter
+        set { numberOfPages = value; }//setter
+    }
+    public BigPhotoAlbum()//DefaultConstructor
+    {
+        numberOfPages = 64;
+    }
+    public BigPhotoAlbum(int pages)//ParamaterisedConstructor
+    {
+        numberOfPages = pages;
+    }
+}
+
+class AlbumTest
+{
+    private int numberOfPages;// field
+    public int NumberOfPages// property
+    {
+        get { return numberOfPages; }//getter
+        set { numberOfPages = value; }//setter
+    }
+    public AlbumTest()//DefaultConstructor
+    {
+        numberOfPages = 24;
+    }
+    public AlbumTest(int pages)//ParamaterisedConstructor
+    {
+        numberOfPages = pages;
+    }
+    public static void Main()
+    {
+        PhotoAlbum pa = new PhotoAlbum();
+        BigPhotoAlbum ba = new BigPhotoAlbum();
+        AlbumTest at = new AlbumTest();
+        Console.WriteLine("In PhotoAlbum number of pages is {0}",pa.NumberOfPages);
+        Console.WriteLine("In BigPhotoAlbum number of pages is {0}", ba.NumberOfPages);
+        Console.WriteLine("In AlbumTest number of pages is {0}", at.NumberOfPages);
+    }
+}
+
+/*--------------------------------------------------------------------------*/
+/* 7. Create a C# program that prompts the user for three names of people 
+  and stores them in an array of Person-type objects. 
+  There will be two people of the Student type and one person of the Teacher type.
+
+  To do this, create a Person class that has a Name property of type string, 
+  a constructor that receives the name as a parameter and and function to print the name.
+
+  Then create two more classes that inherit from the Person class, 
+  they will be called Student and Teacher. 
+  The Student class has a Study method that writes by console that the student is studying. 
+  The Teacher class will have an Explain method that writes to the console that the teacher is explaining. 
+  Remember to also create two constructors on the child classes that call the parent constructor of the Person class.
+
+End the program by reading the people (the teacher and the students) and execute the Explain and Study methods.
+Input:
+
+    Juan
+    Sara
+    Carlos
+
+Output:
+
+    Explain
+    Study
+    Study   */
+
+
+//public class sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        Person[] persons = new Person[3];
+//        int size = persons.Length;
+
+//        for (int i = 0; i < size; i++)
+//        {
+//            if (i == 0)
+//            {
+//                persons[i] = new Teacher(Console.ReadLine());
+//            }
+//            else
+//            {
+//                persons[i] = new Student(Console.ReadLine());
+//            }
+//        }
+
+//        for (int i = 0; i < size; i++)
+//        {
+//            if (i == 0)
+//            {
+//                ((Teacher)persons[i]).Explain();
+
+//            }
+//            else
+//            {
+//                ((Student)persons[i]).Study();
+//            }
+//        }
+//    }
+
+//    public class Person
+//    {
+//        protected string Name { get; set; }
+
+//        public Person(string name)
+//        {
+//            Name = name;
+//        }
+
+//        public override string ToString()
+//        {
+//            return "Hello! My name is " + Name;
+//        }
+
+//        ~Person()
+//        {
+//            Name = string.Empty;
+//        }
+//    }
+
+//    public class Teacher : Person
+//    {
+//        public Teacher(string name) : base(name)
+//        {
+//            Name = name;
+//        }
+
+//        public void Explain()
+//        {
+//            Console.WriteLine("Explain");
+//        }
+//    }
+
+//    public class Student : Person
+//    {
+//        public Student(string name) : base(name)
+//        {
+//            Name = name;
+//        }
+
+//        public void Study()
+//        {
+//            Console.WriteLine("Study");
+//        }
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/* 8. declare a delegate which will take 2 parameter of integer type 
+ * and then then implement the same in two classes one doing the addition 
+ * and another subtraction */
+
+//class class1
+//{
+//    public int fun(int a, int b)
+//    {
+//        return a + b;
+//    }
+//}
+//class class2
+//{
+//    public int fun(int a, int b)
+//    {
+//        return a - b;
+//    }
+//}
+//class sample
+//{
+//    public delegate int myFun(int a, int b);
+//    public static void Main()
+//    {
+//        class1 c1 = new class1();
+//        class2 c2 = new class2();
+//        myFun d;
+//        d = new myFun(c1.fun);
+//        Console.WriteLine(d.Invoke(5, 7));
+//        d = new myFun(c2.fun);
+//        Console.WriteLine(d.Invoke(5, 7));
+//    }
+//}
+
+
+/* 9. Write C# program to find first and last digit of any number   */
+
+//class sample
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int num = Convert.ToInt32(Console.ReadLine());
+//        List<int> digit = new List<int>();
+//        int temp = num;
+//        while (num > 0)
+//        {
+//            int rem = num % 10;
+//            digit.Add(rem);
+//            num /= 10;
+//        }
+
+//        Console.WriteLine("First digit of {0} is {1}",temp, digit[digit.Count - 1]);
+//        Console.WriteLine("Last digit of {0} is {1}",temp, digit[0]);
+//    }
+//}
+
+/* 10. Create a C# program that implements an IVehiculo interface with two methods, 
+ * one for Drive of type void and another for Refuel of type bool that has 
+ * a parameter of type integer with the amount of gasoline to refuel. 
+ * Then create a Car class with a builder that receives a parameter 
+ * with the car's starting gasoline amount and 
+ * implements the Drive and Refuel methods of the car.
+
+The Drive method will print on the screen that the car is Driving, 
+if the gasoline is greater than 0. 
+The Refuel method will increase the gasoline of the car and return true.
+
+To carry out the tests, 
+create an object of type Car with 0 of gasoline in the Main of the program 
+and ask the user for an amount of gasoline to refuel, 
+finally execute the Drive method of the car.
+Input
+
+    50
+
+Output
+
+    Driving */
+
+//interface IVehiculo
+//{
+//    public void Drive();
+//    public bool Refuel(int amt);
+//}
+
+//class Car : IVehiculo
+//{
+//    int Fuel { get; set;}
+
+//    public Car(int fuel)//ParamaterisedConstructor
+//    {
+//        Fuel = fuel;
+//    }
+
+//    public Car()//NoArgsConstructor
+//    {
+//        Fuel = 0;
+//    }
+
+//    public void Drive()
+//    {
+//        if(Fuel>0)
+//            Console.WriteLine("Driving");
+//        else
+//            Console.WriteLine("Not Fuel");
+//    }
+
+//    public bool Refuel(int amt)
+//    {
+//        Fuel += amt;
+//        return true;
+//    }
+//}
+
+//class sample
+//{
+//    public static void Main()
+//    {
+//        Car c = new Car(0);
+//        Console.WriteLine("How much amount of fuel you want to fill:");
+//        int fuel = Convert.ToInt32(Console.ReadLine());//50
+//        if (c.Refuel(fuel))
+//            c.Drive();
+//    }
+//}
---
+++ 01_C# Fundamentals/Test_Set1/Test_Set1/Program.cs
@@ -0,0 +1,403 @@
+﻿/* 1. Write a C# Sharp program that takes a number as input and print its multiplication table.
+ use while loop
+ Test Data:
+ Enter the number: 5
+ Expected Output:
+ 5 * 1 = 5
+ 5 * 2 = 10
+ 5 * 3 = 15....5 * 10 = 50  */
+
+
+//public class Sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        int temp = 1;
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+
+//        while (temp <= 10)
+//        {
+//            Console.WriteLine("{0} * {1} = {2}", n, temp, n * temp);
+//            temp++;
+//        }
+
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+
+/* 2. Write a C# program to count a specified number in a given array of integers.
+ use while loop
+ Input an integer: 5
+ Sample Output
+ Number of 5 present in the said array: 2   */
+
+//public class Sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        int[] arr = { 1, 5, 3, 5, 4 };//array
+//        int temp = arr.Length - 1, count = 0;
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        while (temp >= 0)
+//        {
+//            if (arr[temp] == n)
+//            {
+//                count++;
+//            }
+//            temp--;
+//        }
+//        Console.WriteLine("Number of {0} present in the said array: {1}",n ,count);
+//    }
+//}
+
+/*--------------------------------------------------------------------------*/
+/* 3. Write a program in C# Sharp to display the pattern like right angle triangle using an asterisk.
+ use any loop
+
+ *
+ **
+ ***
+ ****   */
+
+//public class Sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        for (int i = 1; i <= n; i++)
+//        {
+//            for (int j = 1; j <= i; j++)
+//            {
+//                Console.Write("* ");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 4. Write a program in C# Sharp to display the sum of the series [ 9 + 99 + 999 + 9999 ...].
+ Test Data :
+ Input the number or terms :5
+ Expected Output :
+ 9 99 999 9999 99999
+ The sum of the series = 111105 */
+
+//public class Sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());//5
+//        Console.Write("Series : ");
+//        int s = 0, sum = 0;
+//        for (int i = 0; i < n; i++)
+//        {
+//            s = s * 10 + 9;
+//            Console.Write(s + " ");
+//            sum += s;
+//        }
+//        Console.WriteLine();
+//        Console.WriteLine("The sum of the series = {0}", sum);//<--
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 5. Write a program in C# Sharp to display the number in reverse order.
+ Test Data :
+ Input a number: 12345
+ Expected Output :
+ The number in reverse order is : 54321 */
+
+//public class Sample
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());//12345
+//        int rem = 1, rev = 0;
+
+//        while (n > 0)
+//        {
+//            rem = n % 10;//lastDigit
+//            n = n / 10;//Without_lastDigit
+//            rev = (rev * 10) + rem;//reverseNo
+//        }
+
+//        Console.WriteLine("The number in reverse order is : " + rev);//54321
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 6. Write a program in C# Sharp to create a function to calculate the result of raising an integer number to another.
+ Test Data :
+ Input Base number: 3
+ Input the Exponent : 2
+ Expected Output :
+ So, the number 3 ^ (to the power) 2 = 9    */
+
+//public class Sample
+//{
+//    public static int fun(int b, int e)
+//    {
+//        int result = 1;
+//        for (int i = 1; i <= e; i++)
+//        {
+//            result *= b;
+//        }
+//        return result;
+//    }
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the Base number: ");
+//        int b = Convert.ToInt32(Console.ReadLine());//3
+//        Console.WriteLine("Enter the Exponent number: ");
+//        int e = Convert.ToInt32(Console.ReadLine());//2
+//        Console.WriteLine("{0} ^ {1} = {2}", b, e, fun(b, e));//3^2
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 7. Write a program in C# Sharp to create a function to check whether a number is prime or not.
+ Test Data :
+ Input a number : 31
+ Expected Output :
+ 31 is a prime number   */
+
+//public class Sample
+//{
+//    public static void fun(int n)
+//    {
+//        int flag = 0;
+//        for (int i = 2; i < n / 2; i++)
+//        {
+//            if (n % i == 0)
+//            {
+//                flag = 1;
+//                break;
+//            }
+//        }
+//        if (n < 2)
+//            Console.WriteLine(n + " is neither Prime nor Composite");
+//        else if (flag == 0)
+//            Console.WriteLine(n + " is a Prime number");
+//        else
+//            Console.WriteLine(n + " is a Composite number");
+//    }
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        fun(n);
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 8. Create a new project, and include in it the class Person which has a property age 
+  and a function greet and setAge .
+
+     Create a class "Student" and another class "Teacher", both descendants of "Person".
+     The class "Student" will have a public method "GoToClasses", which will write on screen "I’m going to class."
+     
+     The class "Teacher" will have a public method "Explain", which will show on screen "Explanation begins". 
+     Also, it will have a private attribute "subject", a string.
+
+     The class Person must have a method "SetAge (int n)" which will indicate the value of their age (eg, 20 years old).
+     The student will have a public method "ShowAge" which will write on the screen "My age is: 20 years old" (or the corresponding number).
+
+     You must create another test class called "StudentAndTeacherTest" that will contain "Main" and:
+         Create a Person and make it say hello
+         Create a student, set his age to 21, tell him to Greet and display his age
+         Create a teacher, 30 years old, ask him to say hello and then explain. */
+
+
+//class Person
+//{
+//    public int age;
+//    public void greet()
+//    {
+//        Console.WriteLine("hello");
+//    }
+//    public void setAge(int a)
+//    {
+//        age = a;
+//    }
+//}
+
+//class Student : Person
+//{
+//    public void GoToClasses()
+//    {
+//        Console.WriteLine("I'm going to class.");
+//    }
+//    public void ShowAge()
+//    {
+//        Console.WriteLine("My age is: " + age + " years old");
+//    }
+//}
+
+//class Teacher : Person
+//{
+//    private string subject;
+//    public void Explain()
+//    {
+//        Console.WriteLine("Explanation begins");
+//    }
+//}
+
+//class StudentAndTeacherTest
+//{
+//    static void Main(String[] args)
+//    {
+//        Person p = new Person();
+//        p.greet();
+//        Student s = new Student();
+//        s.setAge(21);
+//        s.ShowAge();
+//        Teacher t = new Teacher();
+//        t.setAge(30);
+//        t.greet();
+//        t.Explain();
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 9. Create a class named "Table".It must have a constructor, indicating the width and height of the board. 
+  It will have a method "ShowData" which will write on the screen the width and that height of the table. 
+  Create an array containing 4 tables, with  sizes between 50 and 200 cm, and display all the data.  */
+
+//class Table
+//{
+//    public int width, height;
+//    public Table(int w, int h)
+//    {
+//        width = w;
+//        height = h;
+//    }
+//    public void ShowData()
+//    {
+//        Console.WriteLine("Table width is " + width + " height is " + height);
+//    }
+//}
+//class test
+//{
+//    static void Main()
+//    {
+//        Table[] arr = new Table[4];
+//        Random rnd = new Random();
+//        //Console.WriteLine(rnd.Next(50, 201));
+
+//        for (int i = 0; i < 4; i++)
+//        {
+//            /*Random value in arr[i]*/ //rnd.Next(50, 201) --> Range 50 to 200
+//            Table t = new Table(rnd.Next(50, 201), rnd.Next(50, 201));
+//            arr[i] = t;
+//            arr[i].ShowData();
+//        }
+
+//        /*NotWorking*/
+//        //foreach (Table t in arr)
+//        //{
+//        //    t = new Table(rnd.Next(50, 201), rnd.Next(50, 201));
+//        //    t.ShowData();
+//        //}
+//    }
+//}
+/*--------------------------------------------------------------------------*/
+/* 10. Create a C# program that prompts the user for three names of people 
+  and stores them in an array of Person-type objects. 
+  There will be two people of the Student type and one person of the Teacher type.
+
+  To do this, create a Person class that has a Name property of type string, 
+  a constructor that receives the name as a parameter and and function to print the name.
+
+  Then create two more classes that inherit from the Person class, 
+  they will be called Student and Teacher. 
+  The Student class has a Study method that writes by console that the student is studying. 
+  The Teacher class will have an Explain method that writes to the console that the teacher is explaining. 
+  Remember to also create two constructors on the child classes that call the parent constructor of the Person class.
+
+End the program by reading the people (the teacher and the students) and execute the Explain and Study methods.
+Input:
+
+    Juan
+    Sara
+    Carlos
+
+Output:
+
+    Explain
+    Study
+    Study   */
+
+
+class Person
+{
+    protected string name;
+    public Person(string n)
+    {
+        name = n;
+    }
+    ~Person()
+    {
+        name = string.Empty;
+    }
+    public override string ToString()
+    {
+        return "Hello! My name is " + name;
+    }
+}
+
+class Student : Person
+{
+
+    public Student(string n) : base(n)
+    {
+        name = n;
+    }
+    public void Study()
+    {
+        Console.WriteLine("student is studying");
+    }
+}
+
+class Teacher : Person
+{
+    public Teacher(string n) : base(n)
+    {
+        name = n;
+    }
+    public void Explain()
+    {
+        Console.WriteLine("the teacher is explaining");
+    }
+}
+
+class sample
+{
+    static void Main(String[] args)
+    {
+        Person[] arr = new Person[3];
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (i == 0)
+            {
+                Person p = new Teacher(Console.ReadLine());
+            }
+            else
+            {
+                Person p = new Student(Console.ReadLine());
+            }
+        }
+        /*NotWorking*/
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == 0)
+            {
+                ((Teacher)arr[i]).Explain();
+            }
+            else
+            {
+                ((Student)arr[i]).Study();
+            }
+        }
+    }
+}
+/*--------------------------------------------------------------------------*/
